613f425e091157b9198ed2523e9cf8d19efe0e2f
William <coe9257@gmail.com>
2023-11-20 15:21:17 -0800

    created tic-tac-toe moves. Now I need to check for win conditions

--- main.js
+++ main.js
@@ -6,9 +6,18 @@ document.addEventListener("click", function(event){
     }
 });
 
+document.addEventListener("click", function(event) {
+    let className = event.target.className;
+    if (className == "x_icon_button" || className == "o_icon_button") {
+        ticTacToeGame.start_game_player_selection(className)
+    }
+})
+
 function createTicTacToeGame(className) {
 
-    let player_choice = null;
+    let player_pick = null;
+    let computer_pick = null;
+    let game_move_status = null;
 
     const board = {
         row_1_square_1: "",
@@ -22,19 +31,54 @@ function createTicTacToeGame(className) {
         row_3_square_3: "",
     }
 
-    function player_select(square) {
-        // console.log(square);
+    function start_game_player_selection(className) {
+        console.log(className, className == "x_icon_button");
+        if (className == "x_icon_button") {
+            player_pick = "X";
+            computer_pick = "O";
+        }else {
+            player_pick = "O";
+            computer_pick = "X";
+        };
+        let parent_container = document.querySelector('.start_game_container');
+        let child = document.querySelector('.icon_container');
+        console.log(parent_container, child);
+        parent_container.removeChild(child);
+        start_game();
     }
 
-    function computer_select() {
+    function player_choice(square) {
+        let class_name_specific = square.classList[0];
+        square.textContent = "X";
+        board[class_name_specific] = "X";
+        computer_move();
+    }
 
+    function computer_move() {
+        let array = [];
+        const squares = document.querySelectorAll('.squares');
+        console.log(squares);
+        squares.forEach(function (element) {
+            if (element.textContent == "") {
+                array.push(element);
+            }
+        });
+        if (array.length != 0) {
+            const number = math_random(array.length);
+            const element_choice = array[number - 1];
+            // console.log(`element_choice: `, element_choice, ` array[number]: ${array[number]}`);
+            element_choice.textContent = computer_pick;
+            const className_computer_pick = element_choice.classList[0];
+            board[className_computer_pick] = "O";
+            console.log(board);
+        }
     }
 
     function test_square_empty(event) {
         const element = query_selector_element(event);
         let element_text = element.textContent;
         if (element_text == "") {
-            player_select(element)
+            player_choice(element)
         }
     }
 
@@ -46,15 +90,22 @@ function createTicTacToeGame(className) {
     }
 
     function start_game() {
-        const body  = document.querySelector('body');
-        const element = createElement(`h1`, `text-align: center; color: black`);
-            body.appendChild(element);
+        document.querySelector('h1').textContent = `PLAYER MOVE!`
+    }
+
+    function math_random(number) {
+        const randomNumber = Math.floor(Math.random() * `${number}`) + 1;
+        return randomNumber;
     }
 
     return {
+        start_game_player_selection,
+        player_choice,
         test_square_empty,
-        player_select,
-        computer_select
+        query_selector_element,
+        start_game,
+        computer_move,
+        math_random
     }
 }
 
